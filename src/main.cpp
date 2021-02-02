4fd50c82945d16845c0b04f917d67cc1d653c513
Adam Swanson <adam.swanson.98@gmail.com>
2021-02-01 21:44:58 -0500

    clean: split up setup() and loop() logic

--- src/main.cpp
+++ src/main.cpp
@@ -7,36 +7,55 @@
 #include "LedControl.h"
 #include "U8g2lib.h"
 
-
 // Display drivers
 LedControl lc(LED_DIN, LED_CLK, LED0_CS, 2);
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0);
 
 // Heart animation frame indices
-int indices[6] = { 0, 1, 2, 3, 2, 1 };
+int indices[6] = {0, 1, 2, 3, 2, 1};
 
 // Resin count
 unsigned short resin = 0;
 
+const int FRAME_DELAY = 100;
+const int FRAME_COUNT = 6;
+const int DISPLAY_COUNT = 3;
+const int ROW_COUNT = 8;
 
 // Core logic
 
-int s, i, k;
-
-void setup() 
+void setup()
 {
-    // LEDs
+    setupLEDs();
+    setupLCD();
+    delay(500);
+    setupMatrices();
+}
+
+void loop()
+{
+    drawLCD();
+    renderHeartAnimation();
+    blinkLEDs();
+    updateResinCount();
+}
+
+void setupLEDs()
+{
     pinMode(LED1, OUTPUT);
     pinMode(LED2, OUTPUT);
     pinMode(LED3, OUTPUT);
+}
 
-    // LCD
+void setupLCD()
+{
     u8g2.begin();
     u8g2.setFont(hl_alyx);
+}
 
-    // Matrices
-    delay(500);
-    for(i=0; i<3; i++)
+void setupMatrices()
+{
+    for (int i = 0; i < DISPLAY_COUNT; i++)
     {
         lc.shutdown(i, false);
         lc.setIntensity(i, 4);
@@ -44,28 +63,39 @@ void setup()
     }
 }
 
-void loop() 
+void drawLCD()
 {
-    // Draw LCD image
     u8g2.clearBuffer();
     u8g2.drawXBMP(ball_left, ball_top, ball_width, ball_height, ball_bits);
-    if(resin > 9) u8g2.drawGlyph((LCD_WIDTH / 2) - 2, LCD_HEIGHT - FONT_OFFSET_Y, (resin / 10) + '0');
-    else u8g2.drawGlyph((LCD_WIDTH / 2) - 2, LCD_HEIGHT - FONT_OFFSET_Y, ':');
+    if (resin > 9)
+        u8g2.drawGlyph((LCD_WIDTH / 2) - 2, LCD_HEIGHT - FONT_OFFSET_Y, (resin / 10) + '0');
+    else
+        u8g2.drawGlyph((LCD_WIDTH / 2) - 2, LCD_HEIGHT - FONT_OFFSET_Y, ':');
     u8g2.drawGlyph(LCD_WIDTH - FONT_WIDTH, LCD_HEIGHT - FONT_OFFSET_Y, (resin % 10) + '0');
     u8g2.sendBuffer();
+}
 
-    // Render heart animation
-    for(s=0; s<6; s++)
+void renderHeartAnimation()
+{
+    for (int frame = 0; frame < FRAME_COUNT; frame++)
     {
-        for(i=0; i<3; i++) for(k=0; k<8; k++) lc.setRow(i, k, heart[indices[(s + i) % 6]][k]);
-        delay(100);
+        for (int display = 0; display < DISPLAY_COUNT; display++)
+            for (int row = 0; row < ROW_COUNT; row++)
+                lc.setRow(display, row, heart[indices[(frame + display) % FRAME_COUNT]][row]);
+        delay(FRAME_DELAY);
     }
+}
 
-    // Blink LEDs
-    digitalWrite(LED1, resin % 2 == 0? HIGH:LOW);
-    digitalWrite(LED2, resin % 3 == 0? HIGH:LOW);
-    digitalWrite(LED3, resin % 5 == 0? HIGH:LOW);
+void blinkLEDs()
+{
+    digitalWrite(LED1, resin % 2 == 0 ? HIGH : LOW);
+    digitalWrite(LED2, resin % 3 == 0 ? HIGH : LOW);
+    digitalWrite(LED3, resin % 5 == 0 ? HIGH : LOW);
+}
 
+void updateResinCount()
+{
     resin++;
-    if(resin > 99) resin = 0;
+    if (resin >= 100)
+        resin = 0;
 }
